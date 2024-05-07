9e597a6284e9b83f71115186a178bb2716984c28
harol <harol@LAPTOP-IQ42RQOH>
2024-05-07 03:12:57 -0500

    First commit

---
+++ src/app/pages/table/table.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from 'src/app/models/product.model';
+import { DataSourceProduct } from './data-Source';
+import { FormControl } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html'
+})
+export class TableComponent implements OnInit {
+
+  dataSource = new DataSourceProduct();
+  columns: string[] = ['id', 'title', 'price', 'cover', 'actions'];
+  total = 0;
+  input = new FormControl('', { nonNullable: true });
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    .subscribe(data => {
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal();
+    });
+
+    this.input.valueChanges
+    .pipe(
+      debounceTime(3000)
+    )
+    .subscribe(value => {
+      this.dataSource.find(value);
+    });
+  }
+
+  update(product: Product) {
+    this.dataSource.update(product.id, { price: 20 });
+  }
+
+
+}
